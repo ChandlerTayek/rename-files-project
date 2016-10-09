0a2cdb7dc34c7124d77ed4ae936de38b7f508af6
Chandler Tayek <chandlertayek@gmail.com>
2016-10-09 00:07:43 -0700

    feat: First commit, get every file from directory

---
+++ rename-files.py
@@ -0,0 +1,10 @@
+import os
+
+def rename_files():
+    #(1) get file name from a folder
+    file_list = os.listdir(r"E:\Udacity\programming-foundations\prank")
+    print(file_list)
+    
+    #(2) for each file, rename filename
+    
+rename_files()

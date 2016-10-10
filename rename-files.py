d4dfcb4e1061f9cfa0e516b6a04b0eb519e81158
Chandler Tayek <chandlertayek@gmail.com>
2016-10-09 17:48:15 -0700

    style: Removed some extra files

--- rename-files.py
+++ rename-files.py
@@ -9,8 +9,8 @@ def rename_files():
     os.chdir(r"E:\Udacity\programming-foundations\rename-files-project\prank")
     #(2) for each file, rename filename
     for file_name in file_list:
-        os.rename(file_name, file_name.strip("0123456789"))
-        print("Old Name - " + file_name.strip("0123456789"))
+        print("Old Name - " + file_name)
         print("New Name - " + file_name.strip("0123456789"))
+        os.rename(file_name, file_name.strip("0123456789"))
     os.chdir(saved_path)
 rename_files()
